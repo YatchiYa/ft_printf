1e62bd9277fb7e31800564649fabd7d158645e98
Youcef ARAB <yarab@e2r12p9.42.fr>
2020-01-08 14:29:41 +0100

    asa

--- ft_parse_c.c
+++ ft_parse_c.c
@@ -6,7 +6,7 @@
 /*   By: yarab <yarab@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/30 09:40:25 by yarab             #+#    #+#             */
-/*   Updated: 2019/12/30 10:28:25 by yarab            ###   ########.fr       */
+/*   Updated: 2020/01/08 14:28:05 by yarab            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,8 @@ void		ft_digits_parsing_c(va_list args, int *size, t_flags flags)
 	char	c;
 
 	c = (char)va_arg(args, int);
+	if (flags.width < 0)
+		flags.width *= -1;
 	if (flags.width != -1)
 	{
 		ft_print_elem(flags.width, 1, ' ', size);
@@ -39,6 +41,8 @@ void		ft_minus_parse_c(va_list args, int *size, t_flags flags)
 	char	c;
 
 	c = (char)(va_arg(args, int));
+	if (flags.width < 0)
+		flags.width *= -1;
 	if (flags.width != -1)
 	{
 		ft_putchar(c, size);
@@ -53,6 +57,8 @@ void		ft_zero_parsing_c(va_list args, int *size, t_flags flags)
 	char	c;
 
 	c = (char)va_arg(args, int);
+	if (flags.width < 0)
+		flags.width *= -1;
 	if (flags.width != -1)
 	{
 		ft_print_elem(flags.width, 1, '0', size);
