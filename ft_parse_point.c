702f648b6cf79971e81f958a6f9dd91c02411432
Youcef ARAB <yarab@e1r11p20.42.fr>
2020-01-11 15:54:36 +0100

    awa

--- ft_parse_point.c
+++ ft_parse_point.c
@@ -6,87 +6,59 @@
 /*   By: yarab <yarab@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/30 09:57:58 by yarab             #+#    #+#             */
-/*   Updated: 2020/01/10 14:50:55 by yarab            ###   ########.fr       */
+/*   Updated: 2020/01/11 15:49:12 by yarab            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./ft_printf.h"
 
-
-void	ft_point_parsing_id_extends_2(t_flags flags, int *size)
+void	ft_point_parsing_s(va_list args, int *size, t_flags flags)
 {
-	if (flags.precision < 0)
-		ft_putchar('0', size);
-	else if (flags.precision == -1)
-	{
-		ft_print_elem(flags.width, 1, ' ', size);
-		ft_putchar('0', size);
-	}
-	else
-	{
-		ft_print_elem(flags.width, flags.precision, ' ', size);
-		ft_print_elem(flags.precision, 0, '0', size);
-	}
-}
+	char	*s;
+	int		i;
+	char	*t;
 
-int	ft_point_parsing_id_extends(t_flags flags, int number, int *size, int n)
-{
-	if (number < 0)
+	t = va_arg(args, char*);
+	s = (t == NULL) ? ft_strdup("(null)") : ft_strdup(t);
+	i = 0;
+	if (flags.precision == -1 && flags.is_prec == '0')
+		;
+	else if (flags.precision < 0 && flags.is_prec == '1')
 	{
-		ft_print_elem(flags.width,
-				flags.precision > n ? flags.precision + 1 : n + 1,
-				' ', size);
-		ft_putchar('-', size);
-		number = number * -1;
-		ft_print_elem(flags.precision, n, '0', size);
-	}
-	else
-	{
-		ft_print_elem(flags.width, flags.precision > n ?
-				flags.precision : n, ' ', size);
-		ft_print_elem(flags.precision, n, '0', size);
-	}
-	return (number);
-}
-
-void	ft_point_parsing_id(char *str, va_list args, int *size, t_flags flags)
-{
-	int	number;
-	int	n;
-
-	n = ft_str_length_format(str[0], args);
-	number = va_arg(args, int);
-	if (number == 0)
-		ft_point_parsing_id_extends_2(flags, size);
-	else
-	{
-		if (flags.blanks == 1 && flags.precision != -1)
-			number = ft_point_parsing_id_extends(flags, number, size, n);
+		ft_print_elem(flags.width > ft_strlen(s) ?
+			flags.width : 0, ft_strlen(s), ' ', size);
+		if (ft_strcmp(s, "(null)") == 0)
+			ft_putstr("(null)", size);
 		else
-		{
-			if (number < 0)
+			while (s[i])
 			{
-				ft_print_elem(flags.width, n + 1, ' ', size);
-				ft_putchar('-', size);
-				number = number * -1;
+				ft_putchar(s[i], size);
+				i++;
 			}
-			else
-				ft_print_elem(flags.width, n, ' ', size);
-		}
-		ft_putnbr(number, size);
 	}
+	else
+	{
+		ft_print_elem(flags.width, flags.precision > ft_strlen(s) ?
+				ft_strlen(s) : flags.precision, ' ', size);
+		while (s[i] && i < flags.precision)
+		{
+			ft_putchar(s[i], size);
+			i++;
+		}
+	}
+	free(s);
 }
 
 void	ft_point_parsing(char *str, va_list args, int *size, t_flags flags)
 {
 	if (str[0] == 's')
-		ft_digits_parsing_s(args, size, flags);
+		ft_point_parsing_s(args, size, flags);
 	else if (str[0] == 'c')
 		ft_digits_parsing_c(args, size, flags);
 	else if (str[0] == 'u')
 		ft_digits_parsing_u(str, args, size, flags);
 	else if (str[0] == 'd' || str[0] == 'i')
-		ft_point_parsing_id(str, args, size, flags);
+		ft_digits_parsing_id(str, args, size, flags);
 	else if (str[0] == 'x')
 		ft_digits_parsing_hexa(str, args, size, flags);
 	else if (str[0] == 'X')
@@ -108,21 +80,6 @@ void	ft_parse_point(char *str, va_list args, int *p, int *size)
 	flags.width = 0;
 	k++;
 	k += ft_fill_precision(args, &str[k], &flags);
-	flags.is_prec = '1';
 	*p = *p + k;
 	ft_point_parsing(str + k, args, size, flags);
-	/*if (flags.precision < 0)
-	{
-		if (str[k] == 's')
-		{
-			if (flags.precision == -1)
-				flags.precision = 0;
-			ft_minus_parsing(str + k, args, size, flags);
-		}
-		else
-			ft_point_parsing(str + k, args, size, flags);
-	}
-	else
-		ft_point_parsing(str + k, args, size, flags);
-	*/
 }
